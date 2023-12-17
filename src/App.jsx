a39be9f7e0cf89c94d21ce70ab025072fba9a798
impronen <impronen@gmail.com>
2023-12-17 08:54:18 +0200

    add initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="mainPage">Main Page</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,7 @@
+export default function MenuBar() {
+  return (
+    <>
+      <h1>This is the menu</h1>
+    </>
+  );
+}
