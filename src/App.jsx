cc410b8fa19ac992f31112eb4493909475eeeacd
impronen <impronen@gmail.com>
2023-12-29 18:23:07 +0200

    place API call to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-import useAPI from "./hooks/useAPI";
+import { useState, useEffect } from "react";
+
+const ShopData = () => {
+  const [productData, setProductData] = useState(null);
+  const [loadingData, setLoadingData] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch("https://fakestoreapi.com/products?limit=5")
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("API responded with an error");
+          }
+          return res.json();
+        })
+        .then((json) => console.log(json))
+        .then((json) => {
+          setProductData(json);
+          setLoadingData(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setLoadingData(false);
+        });
+    };
+    fetchData();
+  }, []);
+
+  return { productData, loadingData, error };
+};
 
 function App() {
-  useAPI();
-
+  const { productData, loadingData, error } = ShopData();
   return (
     <>
       <div className="menu">
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,23 @@
-/* eslint-disable react/prop-types */
+import { useOutletContext } from "react-router-dom";
 
-export default function MainPage({ productData, loadingData, error }) {
+export default function MainPage() {
+  const [productData, loadingData, error] = useOutletContext();
+
   return (
     <>
-      <h1>This is the main page area, where youll be having some products</h1>
+      <h1>This is the main page area, where you'll be having some products</h1>
+
+      {productData && (
+        <ul>
+          {productData.map((product) => (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <p>{product.title}</p>
+              <p>{product.price}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
