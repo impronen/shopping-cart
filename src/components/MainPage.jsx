3f9adeaaaa2ea01c8a12f08dcf1bdeed8fa68144
impronen <impronen@gmail.com>
2023-12-30 16:31:37 +0200

    incomplete conditional rendering

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -4,16 +4,21 @@ import ProductCard from "./ProductCard";
 
 export default function MainPage() {
   const [productData, setProductData] = useOutletContext();
+  const [loadingData, setLoadingData] = useOutletContext();
   console.log(productData);
   return (
     <>
       <h1>This is the main page area, where youll be having some products</h1>
       <div>
-        <ul>
-          {productData.map((product) => (
-            <ProductCard key={product.id} {...product} />
-          ))}
-        </ul>
+        {loadingData ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {productData.map((product) => (
+              <ProductCard key={product.id} {...product} />
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
