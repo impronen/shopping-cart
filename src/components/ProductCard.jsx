5060d48b09e7b43aff0aba6b8e26472cc567c9e3
impronen <impronen@gmail.com>
2024-01-06 16:43:10 +0200

    refine card styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 export default function ProductCard({ title, image, price }) {
   return (
-    <div className="flex flex-col bg-burning-orange max-w-96 min-h-96 m-5 items-center justify-center rounded">
+    <div className="flex flex-col border-8 border-burning-orange border-inherit bg-white max-w-96 min-h-96 m-5 items-center justify-center rounded-3xl">
+      <img src={image} alt={title} className="w-1/2 rounded my-5" />
       <h1 className="text-deep-gray text-center font-bold">{title}</h1>
-      <img src={image} alt={title} className="w-1/2 rounded" />
       <p>{price}</p>
     </div>
   );
